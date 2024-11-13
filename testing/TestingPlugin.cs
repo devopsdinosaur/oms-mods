70db7adc996fc04e266c443a740f4e106a58d133
devopsdinosaur <devopsdinosaur@gmail.com>
2024-11-13 14:33:39 -0500

    Initial commit

---
+++ testing/TestingPlugin.cs
@@ -0,0 +1,91 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using BepInEx.Configuration;
+using HarmonyLib;
+using System;
+using System.Reflection;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Events;
+using TMPro;
+
+public static class PluginInfo {
+
+	public const string TITLE = "Testing";
+	public const string NAME = "testing";
+	public const string SHORT_DESCRIPTION = "Just for debugging";
+
+	public const string VERSION = "0.0.1";
+
+	public const string AUTHOR = "devopsdinosaur";
+	public const string GAME_TITLE = "Old Market Simulator";
+	public const string GAME = "oms";
+	public const string GUID = AUTHOR + "." + GAME + "." + NAME;
+	public const string REPO = "oms-mods";
+
+	public static Dictionary<string, string> to_dict() {
+		Dictionary<string, string> info = new Dictionary<string, string>();
+		foreach (FieldInfo field in typeof(PluginInfo).GetFields((BindingFlags) 0xFFFFFFF)) {
+			info[field.Name.ToLower()] = (string) field.GetValue(null);
+		}
+		return info;
+	}
+}
+
+[BepInPlugin(PluginInfo.GUID, PluginInfo.TITLE, PluginInfo.VERSION)]
+public class TestingPlugin : DDPlugin {
+	private Harmony m_harmony = new Harmony(PluginInfo.GUID);
+
+	private void Awake() {
+		logger = this.Logger;
+		try {
+			DDPlugin.set_log_level(DDPlugin.LogLevel.Debug);
+			this.m_harmony.PatchAll();
+			logger.LogInfo($"{PluginInfo.GUID} v{PluginInfo.VERSION} loaded.");
+		} catch (Exception e) {
+			logger.LogError("** Awake FATAL - " + e);
+		}
+	}
+
+	/*
+	[HarmonyPatch(typeof(), "")]
+	class HarmonyPatch_ {
+		private static bool Prefix() {
+			
+			return true;
+		}
+	}
+
+	[HarmonyPatch(typeof(), "")]
+	class HarmonyPatch_ {
+		private static void Postfix() {
+			
+		}
+	}
+
+	[HarmonyPatch(typeof(), "")]
+	class HarmonyPatch_ {
+		private static bool Prefix() {
+			try {
+
+			} catch (Exception e) {
+				DDPlugin._error_log("** HarmonyPatch_.Prefix ERROR - " + e);
+			}
+			return true;
+		}
+	}
+
+	[HarmonyPatch(typeof(), "")]
+	class HarmonyPatch_ {
+		private static void Postfix() {
+			try {
+
+			} catch (Exception e) {
+				DDPlugin._error_log("** HarmonyPatch_.Postfix ERROR - " + e);
+			}
+		}
+	}
+	*/
+}
